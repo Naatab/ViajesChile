aa2a6a3d7e7409f4453b49c4a02481701cd22f69
nata <naata25@gmail.com>
2022-04-17 00:46:11 -0400

    comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
 
+    // Alerta para cuando se apreta el botón //
+
     $("#Enviar").click(function () {
         alert("Su mensaje fue enviado correctamente. Que tenga un buen día :)");
 
 
     });
 
+    // doble click en la sección "¿Quienes somos?" para que cambien de color //
+
     $(".col-4").dblclick(function () {
         $(this).css({
             "color": "blue",
         });
     });
+
+    // doble click en la palabra "Destacados" para que desaparezcan las tarjetas //
 
     $(".titulo").dblclick(function () {
         $(".fotos").toggle();
